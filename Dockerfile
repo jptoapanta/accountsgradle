5a4c6299b16faed80bf15b5287f9dd8ecec96089
Jean Toapanta <jptoapanta4@espe.edu.ec>
2023-08-27 17:27:26 -0500

    commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:17-jdk-alpine
-EXPOSE 8080
+EXPOSE 8081
 VOLUME /tmp
 COPY build/libs/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- build/generated/sources/annotationProcessor/java/main/ec/edu/espe/banquito/accounts/service/mapper/AccountTransactionMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ec/edu/espe/banquito/accounts/service/mapper/AccountTransactionMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-27T16:59:03-0500",
+    date = "2023-08-27T17:26:25-0500",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
