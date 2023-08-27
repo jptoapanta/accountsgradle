a594b60655da05d349c3d4075a2b87e6df6a4ad4
Jean Toapanta <jptoapanta4@espe.edu.ec>
2023-08-27 16:46:45 -0500

    build
    .

---
+++ build/generated/sources/annotationProcessor/java/main/ec/edu/espe/banquito/accounts/service/mapper/AccountMapperImpl.java
@@ -0,0 +1,91 @@
+package ec.edu.espe.banquito.accounts.service.mapper;
+
+import ec.edu.espe.banquito.accounts.controller.req.AccountReqDto;
+import ec.edu.espe.banquito.accounts.controller.res.AccountResDto;
+import ec.edu.espe.banquito.accounts.model.Account;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-08-27T16:37:15-0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 17.0.8 (Oracle Corporation)"
+)
+@Component
+public class AccountMapperImpl implements AccountMapper {
+
+    @Override
+    public AccountResDto toRes(Account account) {
+        if ( account == null ) {
+            return null;
+        }
+
+        AccountResDto.AccountResDtoBuilder accountResDto = AccountResDto.builder();
+
+        accountResDto.id( account.getId() );
+        accountResDto.uniqueKey( account.getUniqueKey() );
+        accountResDto.codeInternalAccount( account.getCodeInternalAccount() );
+        accountResDto.name( account.getName() );
+        accountResDto.totalBalance( account.getTotalBalance() );
+        accountResDto.availableBalance( account.getAvailableBalance() );
+        accountResDto.blockedBalance( account.getBlockedBalance() );
+        accountResDto.lastInterestCalculationDate( account.getLastInterestCalculationDate() );
+        accountResDto.allowOverdraft( account.getAllowOverdraft() );
+        accountResDto.maxOverdraft( account.getMaxOverdraft() );
+        accountResDto.clientUk( account.getClientUk() );
+        accountResDto.groupUk( account.getGroupUk() );
+        accountResDto.productUk( account.getProductUk() );
+
+        return accountResDto.build();
+    }
+
+    @Override
+    public List<AccountResDto> toRes(List<Account> accounts) {
+        if ( accounts == null ) {
+            return null;
+        }
+
+        List<AccountResDto> list = new ArrayList<AccountResDto>( accounts.size() );
+        for ( Account account : accounts ) {
+            list.add( toRes( account ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Account toReq(AccountReqDto account) {
+        if ( account == null ) {
+            return null;
+        }
+
+        Account.AccountBuilder account1 = Account.builder();
+
+        account1.id( account.getId() );
+        account1.uniqueKey( account.getUniqueKey() );
+        account1.codeInternalAccount( account.getCodeInternalAccount() );
+        account1.codeInternationalAccount( account.getCodeInternationalAccount() );
+        if ( account.getAccountHolderType() != null ) {
+            account1.accountHolderType( Enum.valueOf( Account.AccountHolderType.class, account.getAccountHolderType() ) );
+        }
+        account1.accountHolderCode( account.getAccountHolderCode() );
+        account1.name( account.getName() );
+        account1.totalBalance( account.getTotalBalance() );
+        account1.availableBalance( account.getAvailableBalance() );
+        account1.blockedBalance( account.getBlockedBalance() );
+        if ( account.getState() != null ) {
+            account1.state( Enum.valueOf( Account.State.class, account.getState() ) );
+        }
+        account1.lastInterestCalculationDate( account.getLastInterestCalculationDate() );
+        account1.allowOverdraft( account.getAllowOverdraft() );
+        account1.maxOverdraft( account.getMaxOverdraft() );
+        account1.interestRate( account.getInterestRate() );
+        account1.clientUk( account.getClientUk() );
+        account1.groupUk( account.getGroupUk() );
+        account1.productUk( account.getProductUk() );
+
+        return account1.build();
+    }
+}
---
+++ build/generated/sources/annotationProcessor/java/main/ec/edu/espe/banquito/accounts/service/mapper/AccountTransactionMapperImpl.java
@@ -0,0 +1,68 @@
+package ec.edu.espe.banquito.accounts.service.mapper;
+
+import ec.edu.espe.banquito.accounts.controller.res.AccountTransactionResDto;
+import ec.edu.espe.banquito.accounts.model.AccountTransaction;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-08-27T16:37:16-0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 17.0.8 (Oracle Corporation)"
+)
+@Component
+public class AccountTransactionMapperImpl implements AccountTransactionMapper {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    public List<AccountTransactionResDto> toRes(List<AccountTransaction> accountTransactions) {
+        if ( accountTransactions == null ) {
+            return null;
+        }
+
+        List<AccountTransactionResDto> list = new ArrayList<AccountTransactionResDto>( accountTransactions.size() );
+        for ( AccountTransaction accountTransaction : accountTransactions ) {
+            list.add( toRes( accountTransaction ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public AccountTransactionResDto toRes(AccountTransaction accountTransaction) {
+        if ( accountTransaction == null ) {
+            return null;
+        }
+
+        AccountTransactionResDto.AccountTransactionResDtoBuilder accountTransactionResDto = AccountTransactionResDto.builder();
+
+        accountTransactionResDto.id( accountTransaction.getId() );
+        accountTransactionResDto.uniqueKey( accountTransaction.getUniqueKey() );
+        if ( accountTransaction.getTransactionType() != null ) {
+            accountTransactionResDto.transactionType( accountTransaction.getTransactionType().name() );
+        }
+        accountTransactionResDto.reference( accountTransaction.getReference() );
+        accountTransactionResDto.ammount( accountTransaction.getAmmount() );
+        accountTransactionResDto.balanceAfterTransaction( accountTransaction.getBalanceAfterTransaction() );
+        accountTransactionResDto.creditorBankCode( accountTransaction.getCreditorBankCode() );
+        accountTransactionResDto.creditorAccount( accountTransaction.getCreditorAccount() );
+        accountTransactionResDto.debtorBankCode( accountTransaction.getDebtorBankCode() );
+        accountTransactionResDto.debtorAccount( accountTransaction.getDebtorAccount() );
+        accountTransactionResDto.creationDate( accountTransaction.getCreationDate() );
+        accountTransactionResDto.bookingDate( accountTransaction.getBookingDate() );
+        accountTransactionResDto.valueDate( accountTransaction.getValueDate() );
+        accountTransactionResDto.applyTax( accountTransaction.getApplyTax() );
+        if ( accountTransaction.getState() != null ) {
+            accountTransactionResDto.state( accountTransaction.getState().name() );
+        }
+        accountTransactionResDto.notes( accountTransaction.getNotes() );
+        accountTransactionResDto.account( accountMapper.toRes( accountTransaction.getAccount() ) );
+
+        return accountTransactionResDto.build();
+    }
+}
